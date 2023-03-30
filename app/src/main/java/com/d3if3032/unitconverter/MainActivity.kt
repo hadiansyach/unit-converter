9640047c296bf207f53821957311942469dfbf27
Ihsan Hadiansyach <ihadiansyach@gmail.com>
2023-03-30 15:02:06 +0700

    nambah splashscreen, layout, dan gambar

--- app/src/main/java/com/d3if3032/unitconverter/MainActivity.kt
+++ app/src/main/java/com/d3if3032/unitconverter/MainActivity.kt
@@ -2,10 +2,14 @@ package com.d3if3032.unitconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.d3if3032.unitconverter.databinding.ActivityMainBinding
+import com.d3if3032.unitconverter.databinding.CalculationActivityBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
